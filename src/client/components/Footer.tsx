00219428e399d3966a01d8ebdd72be5b90e54331
unknown <xlanimation@gmail.com>
2021-12-01 23:38:11 -0600

    commit to pull changes

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () =>
             {/* PRE-FOOTER (PARTNERS) HORIZONTAL DIVIDER */}
             <svg viewBox="0 0 1440 84" fill="none" style={{ zIndex: 99 }} xmlns="http://www.w3.org/2000/svg">
                 <rect y="-386" width="1442" height="464" fill="white" />
-                <g clip-path="url(#clip0_179_768)">
+                <g clipPath="url(#clip0_179_768)">
                     <rect width="1440" height="966" fill="white" />
                     <path d="M0 34L1440 74L1442 591L0 564V34Z" fill="#EEC31A" />
                     <path d="M278 24L293 62L-1.66103e-06 62L0 24L278 24Z" fill="#EEC31A" />
@@ -45,16 +45,16 @@ const Footer = () =>
 
             {/* FOOTER HORIZONTAL DIVIDER */}
             <svg viewBox="0 0 1440 84" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_179_768)">
+                <g clipPath="url(#clip0_179_768)">
                     <rect width="1440" height="966" transform="translate(0 -538)" fill="white" />
                     <rect y="17.7087" width="1440" height="410.291" fill="#050236" />
 
                     <g mask="url(#mask0_179_768)">
                         <g filter="url(#filter0_f_179_768)">
-                            <path d="M-128.926 367.554L102.148 318.673L172.068 429.958L-44.6838 501.631L-128.926 367.554Z" fill="#6AE0FD" fill-opacity="0.24" />
+                            <path d="M-128.926 367.554L102.148 318.673L172.068 429.958L-44.6838 501.631L-128.926 367.554Z" fill="#6AE0FD" fillOpacity="0.24" />
                         </g>
                         <g filter="url(#filter1_f_179_768)">
-                            <path d="M1273.39 140.247L1504.46 91.3661L1574.38 202.651L1357.63 274.324L1273.39 140.247Z" fill="url(#paint0_linear_179_768)" fill-opacity="0.1" />
+                            <path d="M1273.39 140.247L1504.46 91.3661L1574.38 202.651L1357.63 274.324L1273.39 140.247Z" fill="url(#paint0_linear_179_768)" fillOpacity="0.1" />
                         </g>
                     </g>
                     <path d="M0 -504L1440 -464L1442 53L0 26V-504Z" fill="#EEC31A" />
@@ -62,20 +62,20 @@ const Footer = () =>
                     <path d="M1395.5 10L1406 42L1298.24 42L1290 10L1395.5 10Z" fill="#287FDD" />
                 </g>
                 <defs>
-                    <filter id="filter0_f_179_768" x="-408.926" y="38.6733" width="860.994" height="742.958" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <filter id="filter0_f_179_768" x="-408.926" y="38.6733" width="860.994" height="742.958" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                        <feFlood floodOpacity="0" result="BackgroundImageFix" />
                         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
                         <feGaussianBlur stdDeviation="140" result="effect1_foregroundBlur_179_768" />
                     </filter>
-                    <filter id="filter1_f_179_768" x="993.388" y="-188.634" width="860.994" height="742.958" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <filter id="filter1_f_179_768" x="993.388" y="-188.634" width="860.994" height="742.958" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                        <feFlood floodOpacity="0" result="BackgroundImageFix" />
                         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
                         <feGaussianBlur stdDeviation="140" result="effect1_foregroundBlur_179_768" />
                     </filter>
                     <linearGradient id="paint0_linear_179_768" x1="1388.92" y1="115.806" x2="1419.68" y2="261.198" gradientUnits="userSpaceOnUse">
-                        <stop stop-color="#D52A56" />
-                        <stop offset="0.0001" stop-color="#D52A56" />
-                        <stop offset="1" stop-color="#F98B27" />
+                        <stop stopColor="#D52A56" />
+                        <stop offset="0.0001" stopColor="#D52A56" />
+                        <stop offset="1" stopColor="#F98B27" />
                     </linearGradient>
                     <clipPath id="clip0_179_768">
                         <rect width="1440" height="966" fill="white" transform="translate(0 -538)" />
--- src/client/components/RecipeComp.tsx
+++ src/client/components/RecipeComp.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const RecipeComp = (props: any) =>
 {
+
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() =>
+    {
+        fetch('/api/recipes/random')
+            .then(res => res.json())
+            .then(recipes => setRecipes(recipes))
+    }, []);
+
+    console.log(recipes);
+
     return (
         <>
             <section className="recComp">
@@ -18,19 +32,23 @@ const RecipeComp = (props: any) =>
                             </div>
                             <div className="six columns">
                                 <div className="container">
-                                    <div className="five columns">
-                                        <div className="recwrap">
-                                            <div className="recipe">
-                                                <div className="image" style={{ backgroundImage: `url(/images/pepperSteak.jpg)` }}></div>
-                                                <div className="text">
-                                                    <h2>Pepper Steak</h2>
-                                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla ultrices dapibus. Vivamus eget tellus id ligula posuere malesuada vitae volutpat purus.</p>
-                                                    <Link to="">View Recipe <FontAwesomeIcon icon="arrow-right" /></Link>
+                                    {recipes.map((r, index) => (
+                                        <>
+                                            <div className="five columns">
+                                                <div className="recwrap">
+                                                    <div className="recipe">
+                                                        <div className="image" style={{ backgroundImage: `url(${r.imagelink})` }}></div>
+                                                        <div className="text">
+                                                            <h2>{r.title}</h2>
+                                                            <p>{r.description}</p>
+                                                            <a href={`/recipes/${r.id}`}>View Recipe <FontAwesomeIcon icon="arrow-right" /></a>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div className="five columns">
+                                        </>
+                                    ))}
+                                    {/* <div className="five columns">
                                         <div className="recwrap">
                                             <div className="recipe">
                                                 <div className="image" style={{ backgroundImage: `url(/images/veganPasta.jpg)` }}></div>
@@ -41,7 +59,7 @@ const RecipeComp = (props: any) =>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> */}
                                 </div>
                             </div>
                         </div>

231734565fa5fb69f3afc8a5de7ee3f69d5335b8
edict <acclinate@ps640097.dreamhostps.com>
2021-12-08 07:14:21 -0800

    updates

---
+++ server.ts
@@ -0,0 +1,14 @@
+import * as express from 'express';
+import * as path from 'path';
+import apiRouter from './src/server/routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRouter);
+app.use("/*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port: ${port}`));

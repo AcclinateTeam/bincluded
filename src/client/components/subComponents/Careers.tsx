81bf45a17a804a5feb27cf77346dc989dccd2a8d
acclinateteam <hello@nowincluded.com>
2022-01-12 18:19:21 -0600

    images update

--- src/client/components/subComponents/Careers.tsx
+++ src/client/components/subComponents/Careers.tsx
@@ -9,7 +9,7 @@ const Careers = () =>
                 <a href="https://www.globalteamstaffing.com/copy-of-employee-portal">
                     <div className="panes">
                         <div className="pane">
-                            <div className="image" style={{ backgroundImage: `url("/images/partners/christ-health.png")`, backgroundColor: '#ffffff' }}></div>
+                            <div className="image" style={{ backgroundImage: `url("/images/partners/GlobalTeamStaffing.jpeg")`, backgroundColor: '#ffffff' }}></div>
                             <h2>Global Team Staffing</h2>
                             <p>A reliable staffing company that serves many industries.</p>
                         </div>
@@ -20,7 +20,7 @@ const Careers = () =>
                 <a href="https://www.expresspros.com/SBirminghamAL/Job-Openings.aspx">
                     <div className="panes">
                         <div className="pane">
-                            <div className="image" style={{ backgroundImage: `url("/images/partners/rrpark.jpg")`, backgroundColor: '#ffffff' }}></div>
+                            <div className="image" style={{ backgroundImage: `url("/images/partners/ExpressEmploymentPros.jpeg")`, backgroundColor: '#ffffff' }}></div>
                             <h2>Express Employment Professionals</h2>
                             <p>A leading staffing provider helping job seekers find work with a wide variety of local businesses.</p>
                         </div>
@@ -31,7 +31,7 @@ const Careers = () =>
                 <a href="https://www.pridestaff.com/job-seekers/">
                     <div className="panes">
                         <div className="pane">
-                            <div className="image" style={{ backgroundImage: `url("/images/partners/crisis.webp")`, backgroundColor: '#ffffff' }}></div>
+                            <div className="image" style={{ backgroundImage: `url("/images/partners/PrideStaff.png")`, backgroundColor: '#ffffff' }}></div>
                             <h2>Pride Staff</h2>
                             <p>Goal is to help people reach their personal and professional goals and assist job seekers.</p>
                             {/* <Link to="">Read More <FontAwesomeIcon icon="arrow-right" /></Link> */}
@@ -44,7 +44,7 @@ const Careers = () =>
                 <a href="http://dedicatedpersonnel.com/employee/">
                     <div className="panes">
                         <div className="pane">
-                            <div className="image" style={{ backgroundImage: `url("/images/partners/m4a.png")`, backgroundColor: '#ffffff' }}></div>
+                            <div className="image" style={{ backgroundImage: `url("/images/partners/DedicatedPersonnel.png")`, backgroundColor: '#ffffff' }}></div>
                             <h2>Dedicated Personnel</h2>
                             <p>A professional staffing service, independently owned and operated.</p>
                             {/* <Link to="">Read More <FontAwesomeIcon icon="arrow-right" /></Link> */}
@@ -56,7 +56,7 @@ const Careers = () =>
                 <a href="https://birmingham.snelling.com/job-seekers-employment-opportunities/">
                     <div className="panes">
                         <div className="pane">
-                            <div className="image" style={{ backgroundImage: `url("/images/partners/medicaid.png")`, backgroundColor: '#ffffff' }}></div>
+                            <div className="image" style={{ backgroundImage: `url("/images/partners/Snelling.png")`, backgroundColor: '#ffffff' }}></div>
                             <h2>Snelling</h2>
                             <p>Snelling Birmingham specializes in placing great talent in several demanding business sectors.</p>
                             {/* <Link to="">Read More <FontAwesomeIcon icon="arrow-right" /></Link> */}
@@ -68,7 +68,7 @@ const Careers = () =>
                 <a href="https://www.kellyservices.us/">
                     <div className="panes">
                         <div className="pane">
-                            <div className="image" style={{ backgroundImage: `url("/images/partners/medicare.svg")`, backgroundColor: '#ffffff' }}></div>
+                            <div className="image" style={{ backgroundImage: `url("/images/partners/kelly.png")`, backgroundColor: '#ffffff' }}></div>
                             <h2>Kelly Services</h2>
                             <p>Goal is to help bold companies and bright people connect and work together.</p>
                             {/* <Link to="">Read More <FontAwesomeIcon icon="arrow-right" /></Link> */}

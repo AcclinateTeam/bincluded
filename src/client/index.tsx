2a0fd0ad63c9bb1829f6697743127ea5ca44fa5a
unknown <xlanimation@gmail.com>
2022-07-12 16:17:07 -0500

    updated build to remove full story and effect optimization

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import './scss/app';
-import * as FullStory from '@fullstory/browser';
+// import * as FullStory from '@fullstory/browser';
 import ReactGA from 'react-ga';
 import TagManager from 'react-gtm-module'
 
 const trackingID = 'G-99P06YTR29';
 ReactGA.initialize(trackingID);
 
-FullStory.init({ orgId: '169TY2' });
+// FullStory.init({ orgId: '169TY2' });
 
 const tagManagerArgs = {
     gtmId: 'GTM-WH64Z3D'

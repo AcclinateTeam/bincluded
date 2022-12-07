979b2ae6cd9e4d245e559bf96db665d46d20bd20
unknown <xlanimation@gmail.com>
2022-12-07 17:24:13 -0600

    updates

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -20,6 +20,17 @@ const Home = () =>
         window.scrollTo(0, 0)
     }, [])
 
+    const [stories, setStories] = useState([]);
+
+    useEffect(() =>
+    {
+        fetch('api/stories/storyPosts')
+            .then(res => res.json())
+            .then(stories => setStories(stories))
+    }, []);
+
+    console.log(stories);
+
     // Metro Track State & Effect
     const [tracker, setTracker] = useState<any>([]);
     const [actuals, setActuals] = useState<any>([]);
@@ -192,74 +203,32 @@ const Home = () =>
 
                             <div className="ten columns">
                                 <div className="container case">
-                                    {/* HOME-STORY ITEM */}
-                                    <div className="fourth columns rack">
-                                        <Link to="/stories/walladean">
-                                            <div className="frames">
-                                                <div className="frame">
-                                                    <div className="image">
-                                                        <img src='images/media/walladean.webp' alt="" />
+                                    {stories.map((post: any, index) =>
+                                    {
+                                        if (post.id < 5) {
+                                            return (
+                                                <>
+                                                    {/* NEW ROW */}
+                                                    <div className="fourth columns rack">
+                                                        <Link to={`/stories/single/${post.slug}`}>
+                                                            <div className="frames">
+                                                                <div className="frame">
+                                                                    <div className="image">
+                                                                        <img src={`/images/${post.imagelink}`} alt="" />
+                                                                    </div>
+                                                                    <div className="text">
+                                                                        <h1>{post.title}</h1>
+                                                                        <p>{post.subTitle}</p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </Link>
                                                     </div>
-                                                    <div className="text">
-                                                        <h1>Walladean Streeter</h1>
-                                                        <p>Voices From Our Community</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </div>
-
-                                    {/* HOME-STORY ITEM */}
-                                    <div className="fourth columns rack">
-                                        <Link to="/stories/angela">
-                                            <div className="frames">
-                                                <div className="frame">
-                                                    <div className="image">
-                                                        <img src='images/media/angela.webp' alt="" />
-                                                    </div>
-                                                    <div className="text">
-                                                        <h1>Angela Abdur-Rasheed</h1>
-                                                        <p>Voices From Our Community</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </div>
-
-                                    {/* HOME-STORY ITEM */}
-                                    <div className="fourth columns rack">
-                                        <Link to="/stories/willy">
-                                            <div className="frames">
-                                                <div className="frame">
-                                                    <div className="image" style={{ backgroundImage: `url(/images/charger1.jpg)` }}>
-                                                        <img src='images/media/willy.webp' alt="" />
-                                                    </div>
-                                                    <div className="text">
-                                                        <h1>Willy Etheridge</h1>
-                                                        <p>Voices From Our Community</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </div>
-
-                                    {/* HOME-STORY ITEM */}
-                                    <div className="fourth columns rack">
-                                        <Link to="/stories/celeste">
-                                            <div className="frames">
-                                                <div className="frame">
-                                                    <div className="image" style={{ backgroundImage: `url(/images/charger1.jpg)` }}>
-                                                        <img src='images/media/drharris.webp' alt="" />
-                                                    </div>
-                                                    <div className="text">
-                                                        <h1>Dr. Celeste Reese-Willism</h1>
-                                                        <p>Voices From Our Community</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </div>
-
+                                                </>
+                                            )
+                                        }
+                                        
+                                    })}
                                 </div>
                             </div>
                             <div className="ten columns">

8bc59cee5b22f5fe31c5fa58bff06b023f94d09d
phreeknomenal <xlanimation@gmail.com>
2022-12-20 15:43:06 -0600

    creating new staging/development branch and updating home to show changes between stage/dev and main

--- src/client/components/HealthTechTerminal.tsx
+++ src/client/components/HealthTechTerminal.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
-import General from './subComponents/Family/General';
 
 const HealthTech = () =>
 {
